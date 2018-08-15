8cf7c0df260158152512da1528415565057bd51a
allthesignals <wilburnforce@gmail.com>
2018-08-15 13:33:25 -0400

    Upgrade mapbox-gl; cleanup model

--- tests/dummy/mirage/fixtures/layer-groups.js
+++ tests/dummy/mirage/fixtures/layer-groups.js
@@ -15,7 +15,7 @@ export default [
         "before": "place_other",
         "tooltipable": false,
         "tooltipTemplate": "{{type}}",
-        "highlightable": true,
+        "highlightable": false,
         "style": {
           "id": "citymap-amendments-fill",
           "type": "fill",
@@ -88,7 +88,7 @@ export default [
     "title": "Street Lines",
     "legendIcon": "",
     "legendColor": "",
-    "visible": false,
+    "visible": true,
     "meta": {
       "description": "NYC Department of City Planning Technical Review Division",
       "updated_at": "6 April 2018"
@@ -100,6 +100,7 @@ export default [
           "type": "line",
           "source": "digital-citymap",
           "source-layer": "citymap",
+          "highlightable": true,
           "paint": {
             "line-color": "rgba(51, 51, 51, 1)",
             "line-width": {
